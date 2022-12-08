dac4c26a6f010da3dfedcfdded9a92527b07ddda
Tanzim Mokammel <mtanzim@gmail.com>
2022-12-08 18:05:54 -0500

    untested day 8 code

--- day8/main.ts
+++ day8/main.ts
@@ -14,18 +14,58 @@ interface Coord {
   y: number;
 }
 
-function dfs(grid: number[][], marked: Map<Coord, boolean>, current: Coord) {
+function dfs(
+  grid: number[][],
+  marked: Map<Coord, boolean>,
+  current: Coord
+): boolean {
+  const width = grid[0].length;
+  const height = grid.length;
   const { x, y } = current;
-  console.log(grid[y][x]);
+
+  if (x === width || y === height) {
+    return true;
+  }
+
+  marked.set(current, true);
+
+  const left: Coord = { x: x - 1, y };
+  const right: Coord = { x: x + 1, y };
+  const top: Coord = { x, y: y - 1 };
+  const bottom: Coord = { x, y: y + 1 };
+
+  const neighbors: Coord[] = [];
+  // get valid neighbors
+  for (const c of [left, right]) {
+    if (c.x > 0 && c.x < width && !marked.get(c)) {
+      neighbors.push(c);
+    }
+  }
+  for (const c of [top, bottom]) {
+    if (c.y > 0 && c.y < height && !marked.get(c)) {
+      neighbors.push(c);
+    }
+  }
+  // filter neighbors to ensure they are lower
+  const lowerNeighbors = neighbors.filter(
+    (c) => grid[current.y][current.x] > grid[c.y][c.x]
+  );
+  let foundEdge = false;
+  for (const n of lowerNeighbors) {
+    foundEdge = foundEdge || dfs(grid, marked, n);
+  }
+  marked.set(current, false);
+  return foundEdge;
 }
 
 const grid = parse(input);
 console.log(grid);
 
-const initMap = new Map();
 // iterate over inner grid
 for (let y = 1; y < grid.length - 1; y++) {
   for (let x = 1; x < grid[0].length - 1; x++) {
-    dfs(grid, initMap, { x, y });
+    const initMap = new Map();
+    const hasEdge = dfs(grid, initMap, { x, y });
+    console.log({ val: grid[y][x], hasEdge });
   }
 }
