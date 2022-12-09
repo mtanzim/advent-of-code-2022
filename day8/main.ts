cb593e9d64c3f9335c01c93115313f730d3c2d7b
Tanzim Mokammel <mtanzim@gmail.com>
2022-12-08 21:33:06 -0500

    wow built castles

--- day8/main.ts
+++ day8/main.ts
@@ -1,9 +1,3 @@
-const input = `30373
-25512
-65332
-33549
-35390`;
-
 function parse(input: string): number[][] {
   const lines = input.split("\n");
   return lines.map((l) => l.split("").map(Number));
@@ -14,9 +8,10 @@ interface Coord {
   y: number;
 }
 
+/*
 function dfs(
   grid: number[][],
-  marked: Map<Coord, boolean>,
+  marked: Record<string, boolean>,
   current: Coord
 ): boolean {
   const width = grid[0].length;
@@ -27,8 +22,9 @@ function dfs(
   if (x === width - 1 || y === height - 1 || x === 0 || y === 0) {
     return true;
   }
+  const getCoordKey = (c: Coord) => `${c.x}|${c.y}`;
 
-  marked.set(current, true);
+  marked[getCoordKey(current)] = true;
 
   const left: Coord = { x: x - 1, y };
   const right: Coord = { x: x + 1, y };
@@ -38,43 +34,74 @@ function dfs(
   const neighbors: Coord[] = [];
   // get valid neighbors
   for (const c of [left, right]) {
-    if (c.x >= 0 && c.x < width && !marked.get(c)) {
+    if (c.x >= 0 && c.x < width) {
       neighbors.push(c);
     }
   }
   for (const c of [top, bottom]) {
-    if (c.y >= 0 && c.y < height && !marked.get(c)) {
+    if (c.y >= 0 && c.y < height) {
       neighbors.push(c);
     }
   }
   const curValue = grid[current.y][current.x];
   // filter neighbors to ensure they are lower
   const lowerNeighbors = neighbors.filter((c) => curValue > grid[c.y][c.x]);
-  // console.log({ curValue, neighbors, lowerNeighbors });
-  return lowerNeighbors.some((n) => dfs(grid, marked, n));
-  // let foundEdge = false;
-  // for (const n of lowerNeighbors) {
-  //   foundEdge = foundEdge || dfs(grid, marked, n);
-  // }
-  // marked.set(current, false);
-  // return foundEdge;
+  const unmarkedNeighbors = lowerNeighbors.filter(
+    (c) => !marked[getCoordKey(c)]
+  );
+
+  return unmarkedNeighbors.some((n) => dfs(grid, marked, n));
+}
+*/
+
+function traverseX(grid: number[][], { x, y }: Coord, v: number): boolean {
+  // go left
+  const left = grid[y].slice(0, x);
+  const right = grid[y].slice(x + 1, grid[0].length);
+
+  const visible =
+    left.every((curVal) => curVal < v) || right.every((curVal) => curVal < v);
+  console.log({ x, y, left, v, right, visible });
+  return visible;
+}
+
+function traverseY(grid: number[][], { x, y }: Coord, v: number): boolean {
+  // go left
+  const col = grid.map((col) => col[x]);
+  const top = col.slice(0, y);
+  const bottom = col.slice(y + 1, col.length);
+
+  const visible =
+    top.every((curVal) => curVal < v) || bottom.every((curVal) => curVal < v);
+  console.log({ x, y, top, v, bottom, visible });
+
+  return visible;
 }
 
 async function main() {
-  // const input = await Deno.readTextFile("./day8/input.txt");
+  const testInput = `30373
+25512
+65332
+33549
+35390`;
+  const input = await Deno.readTextFile("./day8/input.txt");
   const grid = parse(input);
-  // console.log(grid);
+  console.log(grid);
 
   const width = grid[0].length;
   const height = grid.length;
   let visibleTrees = 2 * width + 2 * height - 4;
 
   // iterate over inner grid
-  for (let y = 1; y < grid.length - 1; y++) {
-    for (let x = 1; x < grid[0].length - 1; x++) {
-      const marked = new Map();
-      const hasEdge = dfs(grid, marked, { x, y });
+  for (let y = 1; y < height - 1; y++) {
+    for (let x = 1; x < width - 1; x++) {
+      const val = grid[y][x];
+      const hasEdge =
+        traverseY(grid, { x, y }, val) || traverseX(grid, { x, y }, val);
+
       if (hasEdge) {
+        console.log({ x, y, val, hasEdge });
+        console.log();
         visibleTrees++;
       }
     }
