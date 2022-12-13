194d6eb3a5b8d8d86eb2d1cca72e35bbc7165097
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2022-12-13 16:12:35 -0500

    part 1 done

--- day13/main.ts
+++ day13/main.ts
@@ -11,29 +11,41 @@ function isArray(v: any): boolean {
   return Array.isArray(v);
 }
 
+interface Result {
+  result: boolean;
+  shouldContinue: boolean;
+}
+
 // is in order?
-function checkPair(left: Entry, right: Entry): boolean {
+function checkPair(left: Entry, right: Entry): Result {
   console.log({ left, right });
 
   if (!isArray(left) && !isArray(right)) {
-    return left < right;
+    if (left === right) {
+      return { result: false, shouldContinue: true };
+    }
+    return { result: left < right, shouldContinue: false };
   }
   if (isArray(left) && isArray(right)) {
     if (
       (left as Array<number>).length === 0 &&
       (right as Array<number>).length === 0
     ) {
-      return false;
+      return { result: false, shouldContinue: true };
     }
     if ((left as Array<number>).length === 0) {
-      return true;
+      return { result: true, shouldContinue: false };
     }
     if ((right as Array<number>).length === 0) {
-      return false;
+      return { result: false, shouldContinue: false };
     }
     const [headLeft, ...tailLeft] = left as Array<number>;
     const [headRight, ...tailRight] = right as Array<number>;
-    return checkPair(headLeft, headRight) || checkPair(tailLeft, tailRight);
+    const { shouldContinue, result } = checkPair(headLeft, headRight);
+    if (shouldContinue) {
+      return checkPair(tailLeft, tailRight);
+    }
+    return { result, shouldContinue: false };
   }
   if (!isArray(left) && isArray(right)) {
     return checkPair([left as number], right);
@@ -50,9 +62,9 @@ async function main() {
   const isOrdered = pairs
     // .slice(1, 2)
     .reduce(
-      (acc, p, i) => (checkPair(p[0], p[1]) ? acc.concat(i + 1) : acc),
+      (acc, p, i) => (checkPair(p[0], p[1]).result ? acc.concat(i + 1) : acc),
       [] as number[]
-    );
+    ).reduce((acc, cur) => acc + cur, 0)
   console.log(isOrdered);
 }
 
