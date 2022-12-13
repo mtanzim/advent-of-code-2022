1b7b714b955ace7aa00d50f3ed4f397717514e38
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2022-12-13 15:04:16 -0500

    .

--- day13/main.ts
+++ day13/main.ts
@@ -1,5 +1,10 @@
-function parse(input: string): any {
-  return input.split("\n\n").map((pair) => pair.split("\n").map(JSON.parse));
+type Pair = Array<number> | number;
+type Pairs = [Pair, Pair];
+
+function parse(input: string): Array<Pairs> {
+  return input
+    .split("\n\n")
+    .map((pair) => pair.split("\n").map((c) => JSON.parse(c))) as Array<Pairs>;
 }
 
 async function main() {
