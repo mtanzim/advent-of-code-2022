ed438a04ef716a4fe2f1799a2261353aae40c49b
Tanzim Mokammel <mtanzim@gmail.com>
2022-12-23 12:16:06 -0500

    day 5: parse crates

--- day5/main.ts
+++ day5/main.ts
@@ -4,11 +4,12 @@ type Move = {
 };
 
 type Stack = string[];
+type CrateMap = Map<number, Stack>;
 
-function parse(input: string): { moves: Move[]; crates: Stack[] } {
-  const [cratesInput, movesInput] = input.split("\n\n");
+function parseMoves(input: string): Move[] {
+  const [_, movesInput] = input.split("\n\n");
   const moveLines = movesInput.split("\n");
-  const moves = moveLines
+  return moveLines
     .map((line) =>
       line
         .split(" ")
@@ -22,12 +23,47 @@ function parse(input: string): { moves: Move[]; crates: Stack[] } {
       }));
       return acc.concat(newMoves);
     }, [] as Move[]);
+}
 
-  console.log(input);
+function parseStacks(input: string): CrateMap {
+  const [stackInput] = input.split("\n\n");
+  const stackLines = stackInput.split("\n");
+  const numStacks = stackLines
+    .at(-1)
+    ?.split(" ")
+    .filter((t) => t !== "")
+    .map(Number)
+    .filter((t) => Number.isInteger(t))
+    .at(-1);
+
+  if (!numStacks) {
+    throw new Error("error parsing stacks");
+  }
+  const stacks: Stack[] = [...Array(numStacks)].map((_) => []);
+  const startIdx = 1;
+  const gap = 4;
+  stackLines.slice(0, -1).forEach((line) => {
+    const tokens = line.split("");
+    let j = 0;
+    for (let i = startIdx; i < numStacks * gap + 1; i += gap) {
+      const container = tokens[i];
+      if (/[A-Z]/.test(container)) {
+        stacks[j].push(container);
+      }
+      j++;
+    }
+  });
+
+  return new Map(stacks.map((s, idx) => [idx + 1, s]));
+}
+
+function parse(input: string): { moves: Move[]; crates: CrateMap } {
+  return { moves: parseMoves(input), crates: parseStacks(input) };
 }
 
 async function main() {
   const { crates, moves } = parse(await Deno.readTextFile("./day5/input.txt"));
+  console.log({ crates, moves });
 }
 
 main();
