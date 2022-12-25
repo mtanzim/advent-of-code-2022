da3ed601f0ab3ed0b6b2eef1d2dc8e3ee6f3c5ef
Tanzim Mokammel <mtanzim@gmail.com>
2022-12-25 14:50:09 -0500

    slow progress to decimal to snafu

--- day25/main.ts
+++ day25/main.ts
@@ -18,9 +18,38 @@ function snafuToDecimal(snafu: string): number {
   const multipliers = [...Array(digits.length)]
     .map((_, idx) => Math.pow(5, idx))
     .toReversed();
+  console.log(multipliers);
   const decipheredDigits = digits.map(decipherWeirdAssSnafuDigits);
   const values = decipheredDigits.map((v, i) => v * multipliers[i]);
   return values.reduce((acc, cur) => acc + cur, 0);
+}
+
+function decimalToSnafu(n: number): number {
+  const findMultipliers = (cur: number, lst: number[]): number[] => {
+    if (n / cur < 1) {
+      return lst;
+    }
+    return findMultipliers(cur * 5, lst.concat(cur));
+  };
+
+  const convertTo5base = (
+    v: number,
+    lst: number[],
+    mlts: number[]
+  ): number[] => {
+    if (v === 0) {
+      return lst;
+    }
+    const [mltHead, ...newMlt] = mlts;
+    const newV = Math.floor(v / mltHead);
+    const newMod = v % mltHead;
+    return convertTo5base(newMod, lst.concat(newV), newMlt);
+  };
+
+  const multipliers = findMultipliers(1, []).toReversed();
+  const base5Dec = convertTo5base(n, [], multipliers);
+  console.log(multipliers);
+  console.log(base5Dec);
 }
 
 async function main() {
@@ -29,6 +58,7 @@ async function main() {
   const snafuValues = lines.map(snafuToDecimal);
   const sum = snafuValues.reduce((acc, cur) => acc + cur, 0);
   console.log(sum);
+  console.log(decimalToSnafu(2022));
 }
 
 main();
