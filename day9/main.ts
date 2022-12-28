4a1e41249b8430244ff7b65e55f60685e5ee4b6b
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2022-12-28 17:06:52 -0500

    wip

--- day9/main.ts
+++ day9/main.ts
@@ -14,8 +14,110 @@ function flattenDir(moves: Move[]): Direction[] {
   return moves.flatMap(([dir, steps]) => [...Array(steps)].map((_) => dir));
 }
 
+type Coord = {
+  x: number;
+  y: number;
+};
+
+type Position = {
+  head: Coord;
+  tail: Coord;
+};
+
+type Tracker = Set<string>;
+
+function coordToString({ x, y }: Coord): string {
+  return `${x}|${y}`;
+}
+
+function traverse(
+  directions: Direction[],
+  currentPos: Position,
+  tracker: Tracker,
+): Tracker {
+  if (directions.length === 0) {
+    return tracker;
+  }
+  const { head, tail } = currentPos;
+  const [dir, ...rest] = directions;
+
+  const newTracker = new Set(tracker);
+  if (!newTracker.has(coordToString(tail))) {
+    console.log(`tail going to ${coordToString(tail)}`);
+    newTracker.add(coordToString(tail));
+  }
+
+  const nextHead: Coord = (() => {
+    const { x, y } = head;
+    switch (dir) {
+      case "D":
+        return { x, y: y - 1 };
+      case "U":
+        return { x, y: y + 1 };
+      case "L":
+        return { x: x - 1, y };
+      case "R":
+        return { x: x + 1, y };
+      default:
+        throw new Error("invalid direction");
+    }
+  })();
+
+  const nextTail: Coord = (() => {
+    const { x: hx, y: hy } = head;
+    const { x: tx, y: ty } = tail;
+    // touching cases: do not move tail
+    if (Math.abs(hx - tx) <= 1 && Math.abs(hy - ty) <= 1) {
+      return tail;
+    }
+    // same column: move vertically
+    if (hx === tx && Math.abs(hy - ty) == 2) {
+      return { x: tx, y: ty - (ty - hy) - 1 };
+    }
+    // same row: move horizontally
+    if (hy === ty && Math.abs(hx - tx) == 2) {
+      return { x: tx - (tx - hx) - 1, y: ty };
+    }
+
+    // diagonal movements
+    if (hy - ty === 2 && hx - tx === 1) {
+      return { x: tx + 1, y: ty + 1 };
+    }
+
+    if (hy - ty === -2 && hx - tx === 1) {
+      return { x: tx + 1, y: ty - 1 };
+    }
+    if (hx - tx === 2 && hy - ty === 1) {
+      return { x: tx + 1, y: ty + 1 };
+    }
+
+    if (hx - tx === 2 && hy - ty === -1) {
+      return { x: tx + 1, y: ty - 1 };
+    }
+
+    if (hx - tx === -2 && hy - ty === 1) {
+      return { x: tx - 1, y: ty + 1 };
+    }
+
+    if (hx - tx === -2 && hy - ty === -1) {
+      return { x: tx - 1, y: ty - 1 };
+    }
+
+    console.error(currentPos);
+    throw new Error("missed a branch");
+  })();
+  return traverse(rest, { head: nextHead, tail: nextTail }, newTracker);
+}
+
 (async function main() {
   const text = await Deno.readTextFile("./day9/input.txt");
   const moves = flattenDir(parse(text));
-  console.log(moves);
+  const startingPos: Coord = { x: 0, y: 0 };
+  const tracker = traverse(
+    moves,
+    { head: startingPos, tail: startingPos },
+    new Set(),
+  );
+  console.log(tracker);
+  console.log(tracker.size);
 })();
