120f19792d98d2e0b73bc0d6d73aaf7e9decef64
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2022-12-29 15:54:09 -0500

    migrating to bigint arghhh

--- day11/main.ts
+++ day11/main.ts
@@ -1,56 +1,58 @@
 type Monkey = {
   // id is also order
   id: number;
-  items: number[];
-  op: (old: number) => number;
-  test: (v: number) => boolean;
+  items: bigint[];
+  op: (old: bigint) => bigint;
+  test: (v: bigint) => boolean;
   throwIfTrue: Monkey["id"];
   throwIfFalse: Monkey["id"];
-  inspections: number;
+  inspections: bigint;
 };
 
 function parse(text: string): Monkey[] {
   const monkeyClumps = text.split("\n\n");
+
   return monkeyClumps.map((clump) => {
     const [idLine, itemsLine, opLine, testLine, trueLine, falseLine] = clump
       .split("\n");
     const id = Number(idLine.split(" ")[1].split(":")[0]);
-    const items = itemsLine.split(":")[1].split(",").map(Number);
+
+    const items = itemsLine.split(":")[1].split(",").map(BigInt);
 
     const getTrueFalseMonkey = (line: string): number =>
       Number(line.split("monkey")[1]);
     const throwIfTrue = getTrueFalseMonkey(trueLine);
     const throwIfFalse = getTrueFalseMonkey(falseLine);
 
-    const op: (n: number) => number = (() => {
+    const op: (n: bigint) => bigint = (() => {
       const opStr = opLine.split("new =")[1].replace("old", "").split(" ")
         .filter((c) => c !== "");
 
       const [op, v] = opStr;
 
-      if (v !== "old" && isNaN(Number(v))) {
-        throw new Error("invalid value supplied");
-      }
+      // if (v !== "old" && isNaN(BigInt(v))) {
+      //   throw new Error("invalid value supplied");
+      // }
       switch (op) {
         case "+":
           return v !== "old"
-            ? (old: number) => old + Number(v)
-            : (old: number) => old + old;
+            ? (old: bigint) => old + BigInt(v)
+            : (old: bigint) => old + old;
         case "*":
           return v !== "old"
-            ? (old: number) => old * Number(v)
-            : (old: number) => old * old;
+            ? (old: bigint) => old * BigInt(v)
+            : (old: bigint) => old * old;
         default:
           throw new Error("invalid operation identified!");
       }
     })();
 
-    const test: (n: number) => boolean = (() => {
-      const divisor = Number(testLine.split("by")[1]);
-      if (isNaN(divisor)) {
-        throw new Error("failed to parse test divisor");
-      }
-      return (n: number) => n % divisor === 0;
+    const test: (n: bigint) => boolean = (() => {
+      const divisor = BigInt(testLine.split("by")[1]);
+      // if (isNaN(divisor)) {
+      //   throw new Error("failed to parse test divisor");
+      // }
+      return (n: bigint) => n % divisor === BigInt(0);
     })();
 
     return {
@@ -60,7 +62,7 @@ function parse(text: string): Monkey[] {
       test,
       throwIfTrue,
       throwIfFalse,
-      inspections: 0,
+      inspections: BigInt(0),
     };
   });
 }
@@ -78,16 +80,33 @@ function getMonkeyStatus(monkeys: Monkey[]): string {
 /**
 mutates!
  */
-function runRound(monkeys: Monkey[], worryDivider: number): void {
+function runRound(monkeys: Monkey[], worryDivider: bigint): void {
   for (const m of monkeys) {
-    const updatedItems = m.items.map((worryLevel) =>
-      Math.floor(m.op(worryLevel) / worryDivider)
-    );
-    updatedItems.forEach((worryLevel) => {
+    // const updatedItems: bigint[] = m.items.map((worryLevel) =>
+    //   m.op(worryLevel) / worryDivider
+    // );
+    for (let i = 0; i < m.items.length; i++) {
+      const worryLevel = m.items[i];
+      m.items[i] = m.op(worryLevel) / worryDivider;
+
+
+      const newWorryLevel = m.items[i];
+      // m.items.forEach((newWorryLevel) => {
       m.inspections++;
-      const throwToMonkey = m.test(worryLevel) ? m.throwIfTrue : m.throwIfFalse;
-      monkeys[throwToMonkey].items.push(worryLevel);
-    });
+      const throwToMonkey = m.test(newWorryLevel) ? m.throwIfTrue : m.throwIfFalse;
+      monkeys[throwToMonkey].items.push(newWorryLevel);
+      // m.inspections++;
+      // const throwToMonkey = m.test(worryLevel) ? m.throwIfTrue : m.throwIfFalse;
+      // monkeys[throwToMonkey].items.push(worryLevel);
+    }
+    // for (let i = 0; i < m.items.length; i++) {
+    //   const worryLevel = m.items[i];
+    //   // m.items.forEach((worryLevel) => {
+    //   m.inspections++;
+    //   const throwToMonkey = m.test(worryLevel) ? m.throwIfTrue : m.throwIfFalse;
+    //   monkeys[throwToMonkey].items.push(worryLevel);
+    //   // });
+    // }
     m.items = [];
   }
 }
@@ -95,15 +114,23 @@ function runRound(monkeys: Monkey[], worryDivider: number): void {
 (async function main() {
   const text = await Deno.readTextFile("./day11/input.txt");
 
-  [{ worryDivider: 3, numRounds: 20 }, { worryDivider: 1, numRounds: 10000 }]
+  [
+    { worryDivider: BigInt(3), numRounds: 20 },
+    // { worryDivider: BigInt(1), numRounds: 10000 },
+  ]
     .forEach(
       ({ worryDivider, numRounds }) => {
         const monkeys: Monkey[] = parse(text);
-        [...Array(numRounds)].forEach((_) => runRound(monkeys, worryDivider));
+        [...Array(numRounds)].forEach((_, idx) => {
+          console.log(`Round: ${idx + 1}`);
+          runRound(monkeys, worryDivider);
+        });
         console.log(getMonkeyStatus(monkeys));
-        const result = monkeys.map((m) => m.inspections).sort((a, b) => b - a)
+        const result = monkeys.map((m) => m.inspections).sort((a, b) =>
+          (a > b) ? -1 : ((a > b) ? 1 : 0)
+        )
           .slice(0, 2)
-          .reduce((acc, cur) => acc * cur, 1);
+          .reduce((acc, cur) => acc * cur, BigInt(1));
         console.log(result);
       },
     );
