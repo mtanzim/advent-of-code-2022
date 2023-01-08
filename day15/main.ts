b0f860b24b5a31dfc03cfe84c2c714c457b4e50d
Tanzim Mokammel <mtanzim@gmail.com>
2023-01-08 15:45:39 -0500

    this looks a bit more promisiing

--- day15/main.ts
+++ day15/main.ts
@@ -57,122 +57,63 @@ function getManhattanDistance(c1: Coord, c2: Coord): number {
   return Math.abs(x1 - x2) + Math.abs(y1 - y2);
 }
 
-const quadrants: Array<
-  { xfn: (n: number) => number }
-> = [
-  {
-    xfn: (x: number) => x + 1,
-    // yfn: (y: number) => y + 1,
-  },
-  {
-    xfn: (x: number) => x - 1,
-    // yfn: (y: number) => y + 1,
-  },
-  // {
-  //   xfn: (x: number) => x + 1,
-  //   yfn: (y: number) => y - 1,
-  // },
-  // {
-  //   xfn: (x: number) => x - 1,
-  //   yfn: (y: number) => y - 1,
-  // },
-];
-
 function populateDeadzones(
   sensor: Sensor,
   beacon: Beacon,
-  mapping: Mapping,
-  y: number,
-  xMax?: number,
-): number[] {
+  xMax: number,
+  yMax: number,
+): {
+  minX: number;
+  maxX: number;
+  minY: number;
+  maxY: number;
+} {
   const mhDistance = getManhattanDistance(sensor, beacon);
-  // const deadZoneCoords: Coord[] = [];
-  const deadZoneXs: number[] = [];
-
-  // const { sensors, beacons } = getCoordsFromMapping(mapping);
-  // const sensorSet = new Set(sensors.map(coordToStr));
-  // const beaconSet = new Set(beacons.map(coordToStr));
-
   const { x: asx, y: asy } = sensor;
-  // TODO: what if array overflows?
-  quadrants.forEach((q) => {
-    for (let x = asx; Math.abs(x - asx) <= mhDistance; x = q.xfn(x)) {
-      if (xMax && (x > xMax || x < 0)) {
-        break;
-      }
-      const curDist = getManhattanDistance({ x, y }, { x: asx, y: asy });
-      if (
-        // !sensorSet.has(coordToStr({ x, y })) &&
-        // !beaconSet.has(coordToStr({ x, y })) &&
-        curDist <= mhDistance
-      ) {
-        deadZoneXs.push(x);
-      }
-    }
-  });
-
-  return deadZoneXs;
+  const minX = Math.max(0, asx - mhDistance);
+  const maxX = Math.min(xMax, asx + mhDistance);
+  const minY = Math.max(0, asy - mhDistance);
+  const maxY = Math.min(yMax, asy + mhDistance);
+  return {
+    minX,
+    maxX,
+    minY,
+    maxY,
+  };
 }
 
 (async function main() {
   const mapping = parse(await Deno.readTextFile("./day15/input.txt"));
 
-  function partA() {
-    const yInterested = 10;
-    const coordSet = new Set();
-    Object.entries(mapping).forEach(
-      ([sensorStr, beacon]) => {
-        console.log(`plotting sensor ${sensorStr}`);
-        const sensor = strToCoord(sensorStr);
-        const deadZoneCoords = populateDeadzones(
-          sensor,
-          beacon,
-          mapping,
-          yInterested,
-        );
-        deadZoneCoords.forEach((c) => {
-          coordSet.add(coordToStr(c));
-        });
-      },
-    );
-    console.log(coordSet.size);
-  }
-  // partA();
-
   (function partB() {
-    const yMax = 4000000;
-    const xMax = 4000000;
+    const yMax = 20;
+    const xMax = 20;
     const xMult = 4000000;
 
-    // console.log(coordSet);
-
-    // const ranges = [...Array(yMax + 1)].forEach((_, y) => {
-    for (let y = 0; y <= yMax; y++) {
-      const deadZonesInY: number[] = Object.entries(mapping).flatMap(
+    (function findStressSignal() {
+      const deadZoneRanges = Object.entries(mapping).flatMap(
         ([sensorStr, beacon]) => {
-          console.log(`plotting sensor ${sensorStr} for y: ${y}`);
           const sensor = strToCoord(sensorStr);
           return populateDeadzones(
             sensor,
             beacon,
-            mapping,
-            y,
             xMax,
+            yMax,
           );
         },
       );
-      const xNotInPlace = [...new Set(deadZonesInY)].sort((
-        a,
-        b,
-      ) => a - b)
-        .slice(0, xMax + 1).find((i, idx) => i !== idx);
-      if (xNotInPlace) {
-        const stressCoord = { x: xNotInPlace - 1, y };
-        const res = stressCoord.x * xMult + stressCoord.y;
-        console.log({ stressCoord, res });
-        break;
-      }
-    }
-    // });
+      console.log({ deadZoneRanges });
+      // const xNotInPlace = [...new Set(deadZonesInY)].sort((
+      //   a,
+      //   b,
+      // ) => a - b)
+      //   .slice(0, xMax + 1).find((i, idx) => i !== idx);
+      // if (xNotInPlace) {
+      //   const stressCoord = { x: xNotInPlace - 1, y };
+      //   const res = stressCoord.x * xMult + stressCoord.y;
+      //   console.log({ stressCoord, res });
+      //   return;
+      // }
+    })();
   })();
 })();
